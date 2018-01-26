308ac43cd05556389f61a1e037dc117618f9c8ec
Gabrielle Lee <Gabrielle.Lee@openlink.com>
2018-01-26 12:35:02 -0600

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                    new Customer {Id = 1, Name = "John Smith"},
+                    new Customer {Id = 2, Name = "Mary Williams"}
+            };
+
+
+            return View( customers );
+        }
+
+        public ActionResult Details( int id )
+        {
+            if( id != 1 && id != 2 )
+            {
+                return HttpNotFound();
+            }
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = id == 1 ? "John Smith" : "Mary Williams"
+            };
+
+            return View( customer );
+        }
+    }
+}
